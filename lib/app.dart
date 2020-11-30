321560068784952d122e66bf4560fbafa538b1d5
Thomas <thomas.arnoux98@gmail.com>
2020-12-01 00:49:39 +0100

    app.dart ok

--- lib/app.dart
+++ lib/app.dart
@@ -1,80 +1,80 @@
-// import 'package:authentication_repository/authentication_repository.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
+  import 'package:authentication_repository/authentication_repository.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter_bloc/flutter_bloc.dart';
 
-// import 'authentication/authentication.dart';
-// import 'home/home.dart';
-// import 'login/login.dart';
-// import 'splash/splash.dart';
-// import 'theme.dart';
+  import 'authentication/authentication.dart';
+  import 'home/home.dart';
+  import 'login/login.dart';
+  import 'splash/splash.dart';
+  import 'theme.dart';
 
-// class App extends StatelessWidget {
-//   const App({
-//     Key key,
-//     @required this.authenticationRepository,
-//   })  : assert(authenticationRepository != null),
-//         super(key: key);
+  class App extends StatelessWidget {
+    const App({
+      Key key,
+      @required this.authenticationRepository,
+    })  : assert(authenticationRepository != null),
+          super(key: key);
 
-//   final AuthenticationRepository authenticationRepository;
+    final AuthenticationRepository authenticationRepository;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return RepositoryProvider.value(
-//       value: authenticationRepository,
-//       child: BlocProvider(
-//         create: (_) => AuthenticationBloc(
-//           authenticationRepository: authenticationRepository,
-//         ),
-//         child: AppView(),
-//       ),
-//     );
-//   }
-// }
+    @override
+    Widget build(BuildContext context) {
+      return RepositoryProvider.value(
+        value: authenticationRepository,
+        child: BlocProvider(
+          create: (_) => AuthenticationBloc(
+            authenticationRepository: authenticationRepository,
+          ),
+          child: AppView(),
+        ),
+      );
+    }
+  }
 
-// class AppView extends StatefulWidget {
-//   @override
-//   _AppViewState createState() => _AppViewState();
-// }
+  class AppView extends StatefulWidget {
+    @override
+    _AppViewState createState() => _AppViewState();
+  }
 
-// class _AppViewState extends State<AppView> {
-//   final _navigatorKey = GlobalKey<NavigatorState>();
+  class _AppViewState extends State<AppView> {
+    final _navigatorKey = GlobalKey<NavigatorState>();
 
-//   NavigatorState get _navigator => _navigatorKey.currentState;
+    NavigatorState get _navigator => _navigatorKey.currentState;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: theme,
-//       navigatorKey: _navigatorKey,
-//       builder: (context, child) {
-//         return BlocListener<AuthenticationBloc, AuthenticationState>(
-//           listener: (context, state) {
-//             switch (state.status) {
-//               case AuthenticationStatus.authenticated:
-//                 _navigator.pushAndRemoveUntil<void>(
-//                   HomePage.route(),
-//                   (route) => false,
-//                 );
-//                 break;
-//               case AuthenticationStatus.unauthenticated:
-//                 _navigator.pushAndRemoveUntil<void>(
-//                   LoginPage.route(),
-//                   (route) => false,
-//                 );
-//                 break;
-//               default:
-//                 break;
-//             }
-//           },
-//           child: child,
-//         );
-//       },
-//       onGenerateRoute: (_) => SplashPage.route(),
-//     );
-//   }
-// }
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        theme: theme,
+        navigatorKey: _navigatorKey,
+        builder: (context, child) {
+          return BlocListener<AuthenticationBloc, AuthenticationState>(
+            listener: (context, state) {
+              switch (state.status) {
+                case AuthenticationStatus.authenticated:
+                  _navigator.pushAndRemoveUntil<void>(
+                    HomePage.route(),
+                    (route) => false,
+                  );
+                  break;
+                case AuthenticationStatus.unauthenticated:
+                  _navigator.pushAndRemoveUntil<void>(
+                    LoginPage.route(),
+                    (route) => false,
+                  );
+                  break;
+                default:
+                  break;
+              }
+            },
+            child: child,
+          );
+        },
+        onGenerateRoute: (_) => SplashPage.route(),
+      );
+   }
+ }
 
-import 'package:authentication_repository/authentication_repository.dart';
+/*import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:appdiet/authentication/authentication.dart';
@@ -147,4 +147,4 @@ class _AppViewState extends State<AppView> {
       onGenerateRoute: (_) => SplashPage.route(),
     );
   }
-}
+}*/
