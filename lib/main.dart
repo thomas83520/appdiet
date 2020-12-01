021ac36693ac48c7843b9f273fafecf5e35a7b76
Thomas <thomas.arnoux98@gmail.com>
2020-12-01 01:32:33 +0100

    UI ok but appleSignin NOK

--- lib/main.dart
+++ lib/main.dart
@@ -34,22 +34,3 @@ void main() async {
       value: appleSignInAvailable,
       child: App(authenticationRepository: AuthenticationRepository())));
 }
-
-/*}*/
-
-// import 'package:authentication_repository/authentication_repository.dart';
-// import 'package:bloc/bloc.dart';
-// import 'package:equatable/equatable.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:appdiet/app.dart';
-// import 'package:appdiet/simpleBlocObserver.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   EquatableConfig.stringify = kDebugMode;
-//   Bloc.observer = SimpleBlocObserver();
-//   runApp(App(authenticationRepository: AuthenticationRepository()));
-// }
